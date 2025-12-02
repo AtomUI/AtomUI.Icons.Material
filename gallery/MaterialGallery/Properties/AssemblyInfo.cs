73f0d174478fa9062cd075aef6ed4914e6a92f64
chinboy <qinware@163.com>
2025-12-02 15:50:15 +0800

    Refactor namespace

--- gallery/MaterialGallery/Properties/AssemblyInfo.cs
+++ gallery/MaterialGallery/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using Avalonia.Metadata;
 
-[assembly: XmlnsPrefix("https://atomui.net/material-icons/gallery", "material-gallery")]
-[assembly: XmlnsDefinition("https://atomui.net/material-icons/gallery", "MaterialGallery.Controls")]
-[assembly: XmlnsDefinition("https://atomui.net/material-icons/gallery", "MaterialGallery.Models")]
-[assembly: XmlnsDefinition("https://atomui.net/material-icons/gallery", "MaterialGallery.Controls.DesignTokens")]
-[assembly: XmlnsDefinition("https://atomui.net/material-icons/gallery", "MaterialGallery.Controls.Themes")]
-[assembly: XmlnsDefinition("https://atomui.net/material-icons/gallery", "MaterialGallery")]
+[assembly: XmlnsPrefix("https://atomui.net/icons/material/icons-gallery", "material-gallery")]
+[assembly: XmlnsDefinition("https://atomui.net/icons/material/icons-gallery", "MaterialGallery.Controls")]
+[assembly: XmlnsDefinition("https://atomui.net/icons/material/icons-gallery", "MaterialGallery.Models")]
+[assembly: XmlnsDefinition("https://atomui.net/icons/material/icons-gallery", "MaterialGallery.Controls.DesignTokens")]
+[assembly: XmlnsDefinition("https://atomui.net/icons/material/icons-gallery", "MaterialGallery.Controls.Themes")]
+[assembly: XmlnsDefinition("https://atomui.net/icons/material/icons-gallery", "MaterialGallery")]
